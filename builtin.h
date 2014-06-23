ddc81bbb0371cee741a7981cb54af622d3f0e219
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-06-23 05:10:42 -0400

    Added builin functions exit and history.
    Exit can correctly return the argument value.
    
    History will call history.dump().

--- builtin.h
+++ builtin.h
@@ -33,16 +33,28 @@ class Manager {
 
     bool Execute(const std::vector<std::string> &tokens);
     int GetReturnValue() const {return return_value_;}
+    bool GetExitFlag   () const {return exit_flag_;}
+    bool ReadHistory   () {
+      if (history_flag_ == true) {
+      history_flag_ = false;
+      return true;
+      } else
+        return false;
+    }
 
   private: //builtin functions:
     int cd();
     int echo();
     int pwd();
+    int history();
+    int exit();
 
   private:
     fsu::HashTable<KeyType, DataType, HashType> builtin_command_table_;
     std::vector<std::string> token_list_;
-    int return_value_;
+    int  return_value_;
+    bool exit_flag_; //true if you want to exit
+    bool history_flag_; //true if you want to read history
 };
 
 }
--- builtin_functions.cpp
+++ builtin_functions.cpp
@@ -1,4 +1,5 @@
 #include <builtin.h>
+#include <sstream>
 
 /*
  * This file contains all the functions that are built in the shell
@@ -8,8 +9,15 @@ namespace builtin
 Manager::Manager() {
   builtin_command_table_.Insert("cd", &Manager::cd);
   builtin_command_table_.Insert("chdir", &Manager::cd);
+
   builtin_command_table_.Insert("echo", &Manager::echo);
   builtin_command_table_.Insert("pwd", &Manager::pwd);
+  builtin_command_table_.Insert("history", &Manager::history);
+
+  builtin_command_table_.Insert("q", &Manager::exit);
+  builtin_command_table_.Insert("exit", &Manager::exit);
+  exit_flag_ = false;
+  history_flag_ = false;
 }
 
 int Manager::cd() {
@@ -32,4 +40,17 @@ int Manager::pwd() {
   std::cout << dir_string << "\n";
   return 0;
 }
+
+int Manager::exit() {
+  int return_value;
+  exit_flag_ = true;
+  std::stringstream buffer(token_list_[1]);
+  buffer >> return_value;
+  return return_value;
 }
+
+int Manager::history() {
+  history_flag_ = true;
+  return 0;
+}
+} //end namespace builtin
