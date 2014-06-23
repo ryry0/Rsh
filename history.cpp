82c9d2b018d55d453628122deb29a2bf680a7873
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2014-06-22 23:49:57 -0400

    Added a history class.
    It keeps track of HISTORY_SIZE commands, and returns them when calling
    getprev getnext.

---
+++ history.cpp
@@ -0,0 +1,102 @@
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <history.h>
+
+History::History() {
+  history_position_ = history_.rend();
+}
+
+History::History(size_t size) : history_max_size_(size) {
+  history_position_ = history_.rend();
+}
+
+History::~History() {
+}
+/*
+ * WriteHistoryToFile
+ * writes the history from the deque into a file
+ */
+bool History::WriteHistoryToFile(const std::string file_name) {
+  std::ofstream output_file;
+  output_file.open(file_name.c_str());
+  if (!output_file)
+    return false;
+
+  for (auto i : history_)
+    output_file << i << "\n";
+
+  output_file.close();
+  return true;
+}
+
+
+/*
+ * AddToHistory
+ * Pushes back the deque with the new input string.
+ * pops the front when it gets too big, so you keep the most recent commands.
+ */
+void History::AddToHistory(const std::string input) {
+   //create a string out of the input with no newlines
+  std::string stripped_string(input);
+  stripped_string.erase(std::remove(stripped_string.begin(),
+        stripped_string.end(), '\n'), stripped_string.end());
+
+  if (history_.size() >= history_max_size_)
+    history_.pop_back();
+
+  history_.push_front(input);
+  history_position_ = history_.rend();
+}
+
+/*
+ * LoadHistory
+ * loads the history from a file and places it into the deque
+ */
+bool History::LoadHistory(const std::string file_name) {
+  std::string input_string;
+  std::ifstream input_file;
+  input_file.open(file_name.c_str());
+  if (!input_file)
+    return false;
+
+  while (getline(input_file,input_string))
+    history_.push_back(input_string);
+
+  history_position_ = history_.rend();
+  return true;
+}
+
+std::string History::GetPrevious() {
+  //check position bounds
+  if(!history_.empty()) {
+    if (history_position_ <= history_.rend() &&
+        history_position_ > history_.rbegin()) {
+      return *--history_position_;
+    }
+    else { //reset the position
+
+      return *history_position_;
+    }
+  }
+  return "";
+}
+
+std::string History::GetNext() {
+  //check position bounds
+  if(!history_.empty()) {
+    if (history_position_ < history_.rend()-1 &&
+        history_position_ >= history_.rbegin()) {
+      return *++history_position_;
+    }
+    else { //reset the position
+      history_position_ = history_.rend();
+      return "";
+    }
+  }
+  return "";
+}
+
+void History::ResetHistoryPosition() {
+  history_position_ = history_.rbegin();
+}
---
+++ history.h
@@ -0,0 +1,32 @@
+#ifndef HISTORY_H_
+#define HISTORY_H_
+#include <deque>
+/*
+ * History class
+ * This class can be used to keep a history of string data.
+ */
+class History {
+  public:
+    History();
+    History(size_t size);
+    ~History();
+
+    void SetHistorySize (size_t size) {history_max_size_ = size;}
+    void AddToHistory   (const std::string input);
+    //write/reads the history from a file
+    bool WriteHistoryToFile(const std::string file_name);
+    bool LoadHistory    (const std::string file_name);
+
+    //functions for asking for previous history entries
+    void ResetHistoryPosition();
+    std::string GetPrevious();
+    std::string GetNext    ();
+
+  private:
+    size_t history_max_size_;
+    std::deque<std::string> history_;
+    std::deque<std::string>::reverse_iterator history_position_;
+    //this variable holds what command in history will be returned by GetPrev,
+    //getNext
+};
+#endif
